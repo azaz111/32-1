f6bbfca80f35730bed9aebd0ac6d677fe454609b
azaz111 <86608104+azaz111@users.noreply.github.com>
2021-08-30 12:47:45 +0300

    Update bash.bash

--- bash.bash
+++ bash.bash
@@ -20,8 +20,8 @@ sleep 2
 # Третий этап -----------------------------------------------------
 cd 
 mkdir AutoRclone
-git clone https://github.com/azaz111/auto-gmail.git
-cd auto-gmail
+git clone https://github.com/azaz111/chiatest1.git
+cd chiatest1
 unzip AutoRclone.zip -d /root/AutoRclone
 sleep 7
 cd
